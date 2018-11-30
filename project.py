540c8a610e6f3ff428d06fa4c3db9ffc7936ffed
DedanTallMister <45295017+DedanTallMister@users.noreply.github.com>
2018-11-30 12:12:42 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
