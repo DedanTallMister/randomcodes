1fc608e8ebf52d6e5a131b79233b625b70806cee
DedanTallMister <45295017+DedanTallMister@users.noreply.github.com>
2018-11-30 12:07:34 +0100

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	return 'Hello World'
+	rest = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(restaurant_id = rest.id)
+	output = ''
+	for item in items:
+		output += item.name
+		output += '<br />'
+	return output
 
 if __name__ == '__main__':
 	app.debug = True
