1db8b92e4438291717de475859eabd326758b377
DedanTallMister <45295017+DedanTallMister@users.noreply.github.com>
2018-12-01 11:23:04 +0100

    Update project.py

--- project.py
+++ project.py
@@ -19,6 +19,8 @@ def HelloWorld():
 	for item in items:
 		output += item.name
 		output += '<br />'
+		output += item.description
+		output += '<br />'
 		output += item.price
 		output += '<br />'
 		output += '<br />'
