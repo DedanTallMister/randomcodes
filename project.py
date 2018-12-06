46c6acc03e8797d669f7949f4da5b65ee5926be4
DedanTallMister <45295017+DedanTallMister@users.noreply.github.com>
2018-12-06 16:00:41 +0100

    Update project.py

--- project.py
+++ project.py
@@ -40,8 +40,16 @@ def newMenuItem(restaurant_id):
 		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
+	if request.method == 'POST':
+		editedItem = MenuItem(name = request.form['edited'], restaurant_id = restaurant_id, menu_id = menu_id)
+		session.add(editedItem)
+		session.commit()
+		return redirect(url_for('DisplayRestaurants', restaurant_id=restaurant_id))
+	else:
+		return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id)
+
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
